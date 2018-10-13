86632b0a6c496b93d03e0ef4909ba2651b6ffd88
demba75009 <dktzoo@LAPTOP-GUBGDJTU.(none)>
2018-10-13 02:58:37 +0200

    social

--- src/app/wall/wall.component.ts
+++ src/app/wall/wall.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { POSTS } from '../mock/postm';
 import { PostService } from '../post.service';
-
+import { Post } from '../model/post';
 @Component({
   selector: 'app-wall',
   templateUrl: './wall.component.html',
@@ -9,7 +8,18 @@ import { PostService } from '../post.service';
 })
 export class WallComponent implements OnInit {
 
-    posts: POSTS[];
+    posts: Post[] = [
+        { id: 1, name: 'Mr. Nice',body:'test',auteur:'de' },
+        { id: 2, name: 'pagala', body:'test',auteur:'de' },
+        { id: 3, name: 'odele',body:'test',auteur:'de' },
+        { id: 4, name: 'oiinn',body:'test',auteur:'de' },
+        { id: 5, name: 'Magneta',body:'test',auteur:'de' },
+        { id: 6, name: 'RubberMan',body:'test',auteur:'de' },
+        { id: 7, name: 'Dynama',body:'test',auteur:'de' },
+        { id: 8, name: 'Dr IQ',body:'test',auteur:'de' },
+        { id: 9, name: 'Magma',body:'test',auteur:'de' },
+        { id: 10, name: 'Tornado',body:'test',auteur:'de' }
+];
     constructor(private PostService: PostService) {
 
     }
