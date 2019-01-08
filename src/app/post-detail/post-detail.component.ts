8a3513d0e5620f1c0f8e82ac5fc0ad5b166f002e
demba75009 <dktzoo@LAPTOP-GUBGDJTU.(none)>
2019-01-08 03:41:57 +0100

    social

--- src/app/post-detail/post-detail.component.ts
+++ src/app/post-detail/post-detail.component.ts
@@ -1,3 +1,4 @@
+//on importe les bibliothéque nécéssaire
 import { Component, OnInit,Input } from '@angular/core';
 import { Post } from '../wall/model/post';
 import { ActivatedRoute } from '@angular/router';
@@ -20,22 +21,26 @@ export class PostDetailComponent implements OnInit {
         private location: Location
     ) {}
   ngOnInit() {
+        //on initialise la fonction
 
       this.getPost();
 
   }
+  //on crée une fonction qui va selectionnez le post choisi par le client
     onSelect(post: Post): void {
         this.selectedPost = post;
     }
-
+//fonction qui sauvegarde les modifications
     save(): void {
         this.postService.updatePost(this.post)
             .subscribe(() => this.goBack());
     }
+
+    //retour en arriere
     goBack(): void {
         this.location.back();
     }
-
+//on crée une fonction qui va cherchez un post spécifique selon son id
     getPost(): void {
         const id = +this.route.snapshot.paramMap.get('id');
         this.postService.getPost(id)
